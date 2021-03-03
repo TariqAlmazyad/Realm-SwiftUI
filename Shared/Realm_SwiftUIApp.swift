32eddf93402c173639d88b9bc1707ff1d8f010fd
Tariq Almazyad <34104180+TariqAlmazyad@users.noreply.github.com>
2021-03-03 08:28:23 +0300

    Initial Commit

---
+++ Shared/Realm_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Tariq Almazyad on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Realm_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
